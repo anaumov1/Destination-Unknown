a44bf17d50b212483b2ce8359cf22445751a067c
JSheleg <jessica.sheleg@gmail.com>
2021-06-18 16:07:09 -0500

    all departure information and id conversions complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,6 +32,18 @@ document.getElementById('datetime').innerHTML = n + ' ' + time;
  var currency = "USD";
  var marketCountry = "US";
  var limitedQuotes = [];
+ var Places = [];
+ var destinationId = [];
+ var originId;
+ var carrierId;
+ var carriers;
+ var quotes;
+ var price;
+ var dateOfDeparture;
+ var travelDestination;
+ var travelCountry;
+ var departurePoint;
+ var departureCountry;
 
 fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/"+marketCountry+"/"+ currency + "/" + resultsLocale +"/" + pointOfOrigin + "/" + destinationPlace + "/"+ departureDate + "/" + returnDate ,{
 	"method": "GET",
@@ -45,38 +57,86 @@ fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices
 })
 .then(function(results){
     console.log(results);
-    console.log(results.Places[0]);
-    console.log(results.Carriers);
-    console.log(results.Quotes);
-    console.log(results.Quotes[10].OutboundLeg.DepartureDate);
-    console.log(results.Quotes.length);
 
-    //take quotes and pull acceptable (direct flight) quotes
-    for (i = 0; i< results.Quotes.length; i++){
-        if(results.Quotes[i].Direct === true){
-            limitedQuotes.push(results.Quotes[i]);
-            //console.log(results.Quotes[i]);
+    //pull carriers from fetch response
+    carriers = results.Carriers;
+    console.log(carriers);
+    console.log(carriers.length);
+
+    //pull quotes from fetch response
+    quotes = results.Quotes;
+    console.log(quotes);
+    console.log(quotes.length);
+
+    //pull places from fetch response
+    places = results.Places
+    console.log(places)
+    console.log(places.length);
+
+    //reduce quotes to only thoses with direct flights
+    for (var i = 0; i< quotes.length; i++){
+        if(quotes[i].Direct === true){
+            limitedQuotes.push(quotes[i]);
+            console.log(limitedQuotes);
             
         }              
     }
-    //iterate through all the quotes to pull the price, departure date and origin, country of arrival and return date
-    console.log(limitedQuotes.length);
-    for (q = 0; q < limitedQuotes.length; q ++){
-        console.log(limitedQuotes[q].MinPrice + " min price ");
-        console.log(limitedQuotes[q].OutboundLeg.CarrierIds[0] + ": carrier id ")
-        console.log(limitedQuotes[q].OutboundLeg.DepartureDate + ": Departure Date ");
-        console.log(limitedQuotes[q].OutboundLeg.DestinationId + ": Destination ID ");
-        console.log(limitedQuotes[q].OutboundLeg.OriginId + ": Origin ID ");
-
-    }
     
+    //iterate through all the quotes to pull the price, departure date and origin, country of arrival and return date
+    for (var q = 0; q < limitedQuotes.length; q ++){
+
+        //price[q]
+        price = limitedQuotes[q].MinPrice
+        console.log(price + " min price ");
+
+        //carrierId[q]
+        carrierId = limitedQuotes[q].OutboundLeg.CarrierIds[0];
+        console.log(carrierId + ": carrier id ")
+
+        //match carrier id from limitedQuote with carrier name from carriers
+        for( var n = 0; n < carriers.length; n++){
+            if(carrierId === carriers[n].CarrierId ){
+                console.log(carriers[n].Name + " Carrier Name " )
+            }
+        }
+
+        //dateOfDeparture[q]
+        dateOfDeparture = limitedQuotes[q].OutboundLeg.DepartureDate
+        console.log( dateOfDeparture + ": Departure Date ");
+
+        //destination id
+        destinationId= limitedQuotes[q].OutboundLeg.DestinationId;
+        console.log(destinationId + ": Destination ID ");
+
+        //origin id
+        originId = limitedQuotes[q].OutboundLeg.OriginId
+        console.log(originId +  ": Origin ID ");
+
+
+        //convert destination id and origin id into names
+        for(var c = 0; c < places.length; c++){
+            if(destinationId === places[c].PlaceId){
+                travelDestination = places[c].Name; 
+                console.log(travelDestination+ ": Location of Travel ")
+                travelCountry = places[c].CountryName;
+                console.log(travelCountry + ": Country of Travel");
+            }
+
+            //compare quoted origin id to PlaceId to find a match
+            if(originId === places[c].PlaceId){
+                departurePoint = places[c].Name;
+                console.log(  departurePoint + " Place of Departure")
+                departureCountry = places[c].CountryName;
+                console.log(departureCountry + " Country of Departure")
+            }
+        }      
+    } 
 
 
 })
 .catch(function(err){
     console.error(err);
 })
-
 
 
 
