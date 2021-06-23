eca496bed0427dae650f006933b7b0cca0979fda
unknown <fizz.zehra14@gmail.com>
2021-06-23 14:36:40 -0500

    added variables for dynamic

---
+++ assets/js/info.js
@@ -0,0 +1,5 @@
+//adding variables
+var flagImg = document.createElement('img');
+var flagContainer = document.getElementById("flag")
+gifImg.setAttribute('src', response.data[0].images.exampledata.url);
+flagContainer.appendChild(flagImg);
